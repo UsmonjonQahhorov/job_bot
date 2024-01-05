71f5c3a64435e271dbf0014f4c022fe2fbbbbe08
Usmonjon Qohhorov <qohhorovusmonjon@gmail.com>
2024-01-05 23:34:32 +0500

    filter middlewares has been commited

--- bot/middlewares/discrimin_filter.py
+++ bot/middlewares/discrimin_filter.py
@@ -5,5 +5,17 @@ from aiogram.utils.exceptions import Throttled
 from aiogram.dispatcher.middlewares import BaseMiddleware
 from aiogram.dispatcher.handler import CancelHandler, current_handler
 
-class DiscriminationMiddleware(BaseException):
-    pass
+
+class DiscriminationMiddleware(BaseMiddleware):
+    async def on_process_message(self, message: types.Message, data: dict):
+        if await self.contains_discrimination(message.text):
+            await message.reply("Iltimos, haqoratli so'zlarni ishlatmang!!!")
+            raise CancelHandler()
+
+
+    async def contains_discrimination(self, text: str) -> bool:
+        word_list = ['suka', 'jalap', 'qotaq', 'qotoq', 'am', 'mol', 'manqa', 'gandon', 'yiban', 'jalab', 'pizdes', 'pizdyuk', 'iflos']
+        if text.lower() in word_list:
+            return True
+        else:
+            return False
--- main.py
+++ main.py
@@ -6,11 +6,13 @@ import bot.handlers
 from db import db
 from db.model import User
 from test import main, send_message_everyday
+from bot.middlewares.discrimin_filter import DiscriminationMiddleware
 
 if __name__ == '__main__':
     db.init()
     # asyncio.run(main())
     # asyncio.run(send_message_everyday())
+    dp.middleware.setup(DiscriminationMiddleware())
     executor.start_polling(dp, skip_updates=True)
     logging.basicConfig(level=logging.INFO)
     loop = asyncio.get_event_loop()
